f815b5a6e398cb76a493b70f41e92008e885b306
José Thiago <jose.thiago@beeteller.com>
2021-05-30 20:30:04 -0300

    Adding chatbot json workflow and fixing identation

--- Api/src/index.js
+++ Api/src/index.js
@@ -1,7 +1,7 @@
-const app = require('./app')
+const app = require('./app');
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 
-app.listen(port,  () =>  {
-  console.log(`The server is running on port ${port}!`);
+app.listen(port, () => {
+	console.log(`The server is running on port ${port}!`);
 });
